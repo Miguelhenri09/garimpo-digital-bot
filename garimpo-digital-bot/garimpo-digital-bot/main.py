a851487b0c21d115cdaae491a39684efeeddcbc3
Miguel Henrique De Oliveira <130713637+Miguelhenri09@users.noreply.github.com>
2025-11-26 16:36:03 -0300

    Add files via upload

---
+++ garimpo-digital-bot/garimpo-digital-bot/main.py
@@ -0,0 +1,17 @@
+import telegram
+import time
+
+TOKEN = "7648778018:AAGxzOaesHp-nHhpoU6kmi3zUYBmIw80rVY"
+CHAT_ID = "@GarimpoDigitalBr"
+
+bot = telegram.Bot(token=TOKEN)
+
+while True:
+    bot.send_message(
+        chat_id=CHAT_ID,
+        text="⏰ Mensagem automática do Garimpo Digital!\nO bot está funcionando perfeitamente!"
+    )
+
+    print("Mensagem enviada!")
+
+    # time.sleep(60)  # espera 60 segundos para enviar de novo
